587fd28e6712d3d4e8b1020e87416c6253e3fb96
jhecohe <jhecohe@gmail.com>
2023-09-19 23:14:10 -0400

    first commit

---
+++ src/main/java/com/sapient/hiring/tech/CustomCache.java
@@ -0,0 +1,46 @@
+package com.sapient.hiring.tech;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomCache<K, V> {
+
+	List<ListCache<K, V>> listCache;
+
+	public CustomCache() {
+		listCache = new ArrayList<>();
+	}
+
+	public void put(K key, V value) {
+		System.out.println(key.getClass());
+
+		if (listCache.isEmpty()) {
+			add(key, value);
+		} else {
+			K k = listCache.get(0).getKey();
+		}
+	}
+
+	public boolean remove(K key) {
+		// TODO implement this method
+		return false;
+	}
+
+	public V get(K key) {
+		for (ListCache<K, V> cache : listCache) {
+			if (cache.getKey().equals(key)) {
+				return cache.getValue();
+			}
+		}
+		return null;
+	}
+
+	private void add(K key, V value) {
+		for (ListCache<K, V> cache : listCache) {
+			if (cache.getKey().equals(key)) {
+				return;
+			}
+		}
+		listCache.add(new ListCache(key, value));
+	}
+}
---
+++ src/main/java/com/sapient/hiring/tech/ListCache.java
@@ -0,0 +1,30 @@
+package com.sapient.hiring.tech;
+
+public class ListCache <K, V>{
+
+	private K key;
+	private V value;
+	
+	public ListCache(K key, V value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
+	
+	public ListCache() {
+		super();
+	}
+
+	public K getKey() {
+		return key;
+	}
+	public void setKey(K key) {
+		this.key = key;
+	}
+	public V getValue() {
+		return value;
+	}
+	public void setValue(V value) {
+		this.value = value;
+	}
+}
